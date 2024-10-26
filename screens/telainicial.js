9dc037ee72adca995d4c42968500c9c3c9bb8278
Cesar Santos <csar.santos18@gmail.com>
2024-10-26 14:17:48 -0500

    feat: ajustes na tela inical e conexão com o bd

--- screens/telainicial.js
+++ screens/telainicial.js
@@ -1,36 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, FlatList, Image, ScrollView } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons'; 
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import axios from 'axios';
 import { useCart } from '../context/CartContext';
 import { useFocusEffect } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-const products = [
-  { id: 1, name: 'Café', price: 3.00, category: 'Bebidas', image: require('../assets/Cafe.png') },
-  { id: 2, name: 'Refrigerante', price: 4.50, category: 'Bebidas', image: require('../assets/refri.png') },
-  { id: 3, name: 'Bolo/fatia', price: 6.00, category: 'Comidas', image: require('../assets/bolo.png') },
-  { id: 4, name: 'Suco', price: 5.00, category: 'Bebidas', image: require('../assets/suco.png') },
-  { id: 5, name: 'Tip-top', price: 2.50, category: 'Bebidas', image: require('../assets/tip-top.png') },
-  { id: 6, name: 'Salgado', price: 4.00, category: 'Comidas', image: require('../assets/salgado.png') },
-  { id: 7, name: 'Halls', price: 1.50, category: 'Outros', image: require('../assets/halls.png') },
-  { id: 8, name: 'Suco e salgado', price: 9.00, category: 'Combos', image: require('../assets/salgado+suco.png') },
-  { id: 9, name: 'Refrigerante e salgado', price: 8.50, category: 'Combos', image: require('../assets/salgado+refri.png') },
-  { id: 10, name: 'Suco e Refrigerante', price: 9.50, category: 'Combos', image: require('../assets/suco+refri.png') },
-  { id: 11, name: 'Refrigerante e Salgado', price: 8.00, category: 'Combos', image: require('../assets/salgado+refri.png') },
-  { id: 12, name: 'Café e Salgado', price: 8.50, category: 'Combos', image: require('../assets/salgado+cafe.png') },
-  { id: 13, name: 'Bolo e Café', price: 8.50, category: 'Combos', image: require('../assets/bolo+cafe.png') },
-  { id: 14, name: 'Produto', price: 8.50, category: 'Outros', image: require('../assets/fundo.png') },
-];
+// Mapeamento das imagens dos produtos
+const imageMapping = {
+  'Coxinha de Frango': require('../assets/coxinha.png'),
+  'Pastel de Frango': require('../assets/pastel_frango.png'),
+  'Pastel de Carne': require('../assets/pastel_carne.png'),
+  'Pastel de Queijo': require('../assets/pastel_queijo.png'),
+  'Hambúrguer': require('../assets/hamburguer.png'),
+  'KitKat': require('../assets/kitkat.png'),
+  'Halls Menta': require('../assets/halls.png'),
+  'Bolo de Chocolate': require('../assets/bolo.png'),
+  'Snickers': require('../assets/snickres.png'),
+  'Coca Cola': require('../assets/coca.png'),
+  'Coca Cola Zero': require('../assets/coca-zero.png'),
+  'Fanta Uva': require('../assets/fanta-uva.png'),
+  'Fanta Laranja': require('../assets/fanta-laranja.png'),
+  'Pepsi': require('../assets/pepsi.png'),
+  'Suco Natural de Laranja': require('../assets/suco-laranja.png'),
+  'Suco Natural de Acerola': require('../assets/suco-acerola.png'),
+  'Suco Natural de Maracuja': require('../assets/suco-maracuja.png'),
+  'Guaraná Antarctica': require('../assets/guarana.png'),
+  'Gatorade': require('../assets/gatorade.png'),
+};
 
-const TelaInicial = ({ navigation }) => {
+const TelaInicial = ({ navigation, userType }) => {
+  const [products, setProducts] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('Todos');
   const { cartItems, addToCart } = useCart();
   const [activePage, setActivePage] = useState('TelaInicial');
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('http://192.168.1.5:5000/produtos');
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar produtos:', error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
   const filteredProducts = selectedCategory === 'Todos'
-    ? products
-    : products.filter(product => product.category === selectedCategory);
+      ? products
+      : products.filter(product => product.id_categoria === selectedCategory);
 
   const handleAddToCart = (item) => {
     addToCart(item);
@@ -43,112 +63,119 @@ const TelaInicial = ({ navigation }) => {
   });
 
   return (
-    <View style={styles.container}>
-      <View style={styles.topSection}>
-        <Text style={styles.title}>Bem-vindo à Tela Inicial!</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Carrinho')} style={styles.cartButton}>
-          <FontAwesome name="shopping-cart" size={24} color="#FFFFFF" />
-          <Text style={styles.cartText}> Carrinho ({totalItemsInCart})</Text>
-        </TouchableOpacity>
-      </View>
+      <View style={styles.container}>
+        <View style={styles.topSection}>
+          <Text style={styles.title}>Bem-vindo à Tela Inicial!</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Carrinho')} style={styles.cartButton}>
+            <FontAwesome name="shopping-cart" size={24} color="#FFFFFF" />
+            <Text style={styles.cartText}> Carrinho ({totalItemsInCart})</Text>
+          </TouchableOpacity>
+        </View>
 
-      <View style={styles.filtersContainer}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.filters}>
-          {['Todos', 'Combos', 'Comidas', 'Bebidas', 'Outros'].map((category) => (
-            <TouchableOpacity 
-              key={category} 
-              onPress={() => setSelectedCategory(category)} 
-              style={[styles.filterButton, { backgroundColor: selectedCategory === category ? '#000066' : '#DEDEDE' }]} 
-            >
-              <Text style={[styles.filterText, { color: selectedCategory === category ? '#FFFFFF' : '#050505' }]}> 
-                {category}
-              </Text>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-      </View>
+        <View style={styles.filtersContainer}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.filters}>
+            {['Todos', 'Combos', 'Comidas', 'Bebidas', 'Outros'].map((category) => (
+                <TouchableOpacity
+                    key={category}
+                    onPress={() => setSelectedCategory(category)}
+                    style={[styles.filterButton, { backgroundColor: selectedCategory === category ? '#000066' : '#DEDEDE' }]}
+                >
+                  <Text style={[styles.filterText, { color: selectedCategory === category ? '#FFFFFF' : '#050505' }]}>
+                    {category}
+                  </Text>
+                </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
 
-      <View style={styles.bottomSection}>
-        <FlatList
-          data={filteredProducts}
-          numColumns={2}
-          renderItem={({ item }) => (
-            <View style={styles.productBox}>
-              <Image source={item.image} style={styles.productImage} />
-              <View style={styles.productDetails}>
-                <Text style={styles.productName}>{item.name}</Text>
-                <View style={styles.addToCartRow}>
-                  <Text style={styles.productPrice}>R$ {item.price.toFixed(2).replace('.', ',')}</Text>
-                  <TouchableOpacity 
-                    style={styles.addButton} 
-                    onPress={() => handleAddToCart(item)} 
-                  >
-                    <FontAwesome name="plus" size={16} color="#FFFFFF" />
-                  </TouchableOpacity>
-                </View>
-              </View>
+        <View style={styles.bottomSection}>
+          <FlatList
+              data={filteredProducts}
+              numColumns={2}
+              renderItem={({ item }) => (
+                  <View style={styles.productBox}>
+                    <Image source={imageMapping[item.nome] || require('../assets/default.png')} style={styles.productImage} />
+                    <View style={styles.productDetails}>
+                      <Text style={styles.productName}>{item.nome}</Text>
+                      <View style={styles.addToCartRow}>
+                        <Text style={styles.productPrice}>
+                          R$ {(
+                            (userType === 'aluno' && item.preco_aluno != null ? parseFloat(item.preco_aluno) :
+                                    userType === 'funcionario' && item.preco_funcionario != null ? parseFloat(item.preco_funcionario) :
+                                        item.preco_base != null ? parseFloat(item.preco_base) : 0
+                            ).toFixed(2).replace('.', ',')
+                        )}
+                        </Text>
+                        <TouchableOpacity
+                            style={styles.addButton}
+                            onPress={() => handleAddToCart(item)}
+                        >
+                          <FontAwesome name="plus" size={16} color="#FFFFFF" />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </View>
+              )}
+              keyExtractor={item => item.id.toString()}
+              contentContainerStyle={{ marginTop: 10, alignItems: 'center' }}
+          />
+        </View>
+
+        {/* Nav Bar */}
+        <View style={styles.navBar}>
+          <TouchableOpacity
+              style={styles.navItem}
+              onPress={() => {
+                setActivePage('TelaInicial');
+                navigation.navigate('TelaInicial');
+              }}
+          >
+            <View style={styles.navItemContainer}>
+              <FontAwesome6 name="house" size={20} color={activePage === 'TelaInicial' ? '#A80000' : '#A2A2A2'} />
             </View>
-          )}
-          keyExtractor={item => item.id.toString()}
-          contentContainerStyle={{ marginTop: 10 }}  
-        />
+            {activePage === 'TelaInicial' && <View style={styles.elipse} />}
+          </TouchableOpacity>
+
+          <TouchableOpacity
+              style={styles.navItem}
+              onPress={() => {
+                setActivePage('Carrinho');
+                navigation.navigate('Carrinho');
+              }}
+          >
+            <View style={styles.navItemContainer}>
+              <FontAwesome name="shopping-cart" size={30} color={activePage === 'Carrinho' ? '#A80000' : '#A2A2A2'} />
+            </View>
+            {activePage === 'Carrinho' && <View style={styles.elipse} />}
+          </TouchableOpacity>
+
+          <TouchableOpacity
+              style={styles.navItem}
+              onPress={() => {
+                setActivePage('Pagamento');
+                navigation.navigate('Pagamento');
+              }}
+          >
+            <View style={styles.navItemContainer}>
+              <FontAwesome name="credit-card" size={24} color={activePage === 'Pagamento' ? '#A80000' : '#A2A2A2'} />
+            </View>
+            {activePage === 'Pagamento' && <View style={styles.elipse} />}
+          </TouchableOpacity>
+
+          <TouchableOpacity
+              style={styles.navItem}
+              onPress={() => {
+                setActivePage('Validação');
+                navigation.navigate('Validação');
+              }}
+          >
+            <View style={styles.navItemContainer}>
+              <Ionicons name="ticket" size={30} color={activePage === 'Validação' ? '#A80000' : '#A2A2A2'} />
+            </View>
+            {activePage === 'Validação' && <View style={styles.elipse} />}
+          </TouchableOpacity>
+        </View>
       </View>
-
-      {/* Nav Bar */}
-      <View style={styles.navBar}>
-        <TouchableOpacity 
-          style={styles.navItem} 
-          onPress={() => {
-            setActivePage('TelaInicial');
-            navigation.navigate('TelaInicial');
-          }}
-        >
-          <View style={styles.navItemContainer}>
-            <FontAwesome6 name="house" size={20} color={activePage === 'TelaInicial' ? '#A80000' : '#A2A2A2'} />
-          </View>
-          {activePage === 'TelaInicial' && <View style={styles.elipse} />}
-        </TouchableOpacity>
-
-        <TouchableOpacity 
-          style={styles.navItem} 
-          onPress={() => {
-            setActivePage('Carrinho');
-            navigation.navigate('Carrinho');
-          }}
-        >
-          <View style={styles.navItemContainer}>
-            <FontAwesome name="shopping-cart" size={30} color={activePage === 'Carrinho' ? '#A80000' : '#A2A2A2'} />
-          </View>
-          {activePage === 'Carrinho' && <View style={styles.elipse} />}
-        </TouchableOpacity>
-
-        <TouchableOpacity 
-          style={styles.navItem} 
-          onPress={() => {
-            setActivePage('Pagamento'); 
-            navigation.navigate('Pagamento'); 
-          }}
-        >
-          <View style={styles.navItemContainer}>
-            <FontAwesome name="credit-card" size={24} color={activePage === 'Pagamento' ? '#A80000' : '#A2A2A2'} style={styles.cardIcon} />
-          </View>
-          {activePage === 'Pagamento' && <View style={styles.elipse} />}
-        </TouchableOpacity>
-
-        <TouchableOpacity 
-          style={styles.navItem} 
-          onPress={() => {
-            setActivePage('');
-            navigation.navigate('Validação');
-          }}
-        >
-          <View style={styles.navItemContainer}>
-            <Ionicons name="ticket" size={30} color={activePage === 'validacao' ? '#A80000' : '#A2A2A2'} />
-          </View>
-          {activePage === 'validacao' && <View style={styles.elipse} />}
-        </TouchableOpacity>
-      </View>
-    </View>
   );
 };
 
@@ -157,6 +184,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F9F9F9',
   },
+
+  // Top Section
   topSection: {
     flex: 0.5,
     backgroundColor: '#1A1A1A',
@@ -178,6 +207,8 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     marginLeft: 5,
   },
+
+  // Filters Section
   filtersContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -185,7 +216,6 @@ const styles = StyleSheet.create({
     height: 60,
     paddingVertical: 5,
     paddingHorizontal: 10,
-    marginTop: 0,
   },
   filterButton: {
     paddingHorizontal: 10,
@@ -199,30 +229,30 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '600',
   },
+
+  // Bottom Section
   bottomSection: {
     flex: 1,
     backgroundColor: '#F9F9F9',
     paddingHorizontal: 16,
-    paddingTop: 0,
     paddingBottom: 1,
   },
   productBox: {
-    flex: 1,
     flexDirection: 'column',
     justifyContent: 'space-between',
     marginBottom: 16,
-    marginRight: 10,
-    marginLeft: 10,
+    marginHorizontal: 8,
     backgroundColor: '#E9E4DE',
     borderRadius: 8,
-    elevation: 1,
     padding: 10,
-    width: '45%',
+    width: '47%',
+    height: 200, // Ajuste a altura para garantir que todos fiquem alinhados
   },
   productImage: {
     width: '100%',
     height: 120,
     borderRadius: 8,
+    resizeMode: 'contain', // Ajuste para manter a proporção da imagem
   },
   productDetails: {
     marginTop: 5,
@@ -231,7 +261,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   productName: {
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: 'bold',
     color: '#242424',
   },
@@ -248,12 +278,14 @@ const styles = StyleSheet.create({
   addButton: {
     backgroundColor: '#000066',
     paddingVertical: 10,
-    paddingHorizontal: 12, 
+    paddingHorizontal: 12,
     borderRadius: 8,
     width: 40,
-    height: 35, 
-    alignItems: 'center', 
+    height: 35,
+    alignItems: 'center',
   },
+
+  // Nav Bar
   navBar: {
     flexDirection: 'row',
     justifyContent: 'space-around',
@@ -267,12 +299,11 @@ const styles = StyleSheet.create({
   navItem: {
     alignItems: 'center',
     position: 'relative',
-    width: 60, 
+    width: 60,
     marginBottom: 10,
   },
-  navText: {
-    fontSize: 12,
-    color: '#A2A2A2',
+  navItemContainer: {
+    alignItems: 'center',
   },
   elipse: {
     position: 'absolute',
@@ -285,5 +316,6 @@ const styles = StyleSheet.create({
     marginLeft: -10,
   },
 });
+
 
 export default TelaInicial;
